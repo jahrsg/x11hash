6f532ef5e00e5d6490f21154a9e5a6f37623ca47
scruffy <scruffy@nakamoto.pw>
2015-05-04 02:38:01 +0300

    test with hash data from x11 miner

--- x11test.c
+++ x11test.c
@@ -1,19 +1,46 @@
 #include "x11.h"
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
-	char res[33];
-	memset(res, 33, 0);
-	if(argc > 1)
-		x11_hash(argv[1], res);
-	else
 	{
 		char in[80];
-		int i;
-		for (i = 0; i < 80; i++)
-			in[i] = i;
-		x11_hash(in, res);
+		char hash[32];
+		char res[32];
+		// haslist.data contains data in format "Data:[input]Hash:[hash]\n" in binary format getted from cpu dashcoin miner
+		FILE* f = fopen("hashlist.data", "rb");
+		if (f)
+		{
+			char buf[128];
+			while (1)
+			{
+				unsigned r = fread(buf, 1, 6, f);
+				if (!r)
+					break;
+				r = fread(in, 1, 80, f);
+				if (!r)
+					break;
+				r = fread(buf, 1, 7, f);
+				if (!r)
+					break;
+				r = fread(hash, 1, 32, f);
+				if (!r)
+					break;
+				r = fread(buf, 1, 2, f);
+				if (!r)
+					break;
+				x11_hash(in, res);
+				if (memcmp(res, hash, 32) != 0)
+				{
+					printf("ERROR, incorrect hash\n");
+				}
+				else
+				{
+					printf("Correct hash\n");
+				}
+			}
+			fclose(f);
+		}
 	}
-	printf("%s", res);
 	return 0;
 }
